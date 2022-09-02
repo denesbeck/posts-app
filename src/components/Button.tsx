1fd523277d18860fe8ac98b66c9020aac0abb91f
Denes Beck <denes.beck1@protonmail.com>
2022-09-02 21:19:25 +0200

    minor changes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,20 +5,15 @@ interface ButtonProps {
 }
 
 function Button({ type, label, handler }: ButtonProps) {
-    const renderButton = () => {
-        switch (type) {
-            case 'primary':
-                return 'bg-blue-400 hover:bg-blue-300'
-            case 'secondary':
-                return 'bg-gray-600 dark:bg-gray-700 rounded hover:bg-gray-500 dark:hover:bg-gray-600'
-            case 'cancel':
-                return 'bg-pink-700 dark:bg-pink-800 dark:hover:bg-pink-700 hover:bg-pink-600'
-        }
+    const buttonStyle = {
+        primary: 'bg-blue-400 hover:bg-blue-300',
+        secondary: 'bg-gray-600 dark:bg-gray-700 rounded hover:bg-gray-500 dark:hover:bg-gray-600',
+        cancel: 'bg-pink-700 dark:bg-pink-800 dark:hover:bg-pink-700 hover:bg-pink-600',
     }
 
     return (
         <button
-            className={`rounded px-4 py-2 font-bold text-white ${renderButton()} outline-2 outline-offset-2 outline-teal-500 focus:outline`}
+            className={`rounded px-4 py-2 font-bold text-white ${buttonStyle[type]} outline-2 outline-offset-2 outline-teal-500 focus:outline`}
             type='button'
             onClick={() => handler()}
         >
